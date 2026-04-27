09c6808ae6562ee51e7d7c4b59c0069726ed6311
Mayank Kar <mayank.kar2021@gmail.com>
2026-04-27 18:23:36 +0530

    Untested, simplified version

--- api.c
+++ api.c
@@ -1,21 +1,23 @@
+#include <error.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct ArgTemplate {
     char *programName;
-    int numShortOptions;
-    char **shortOptions;
-    int numLongOptions;
-    char **longOptions;
+    int numOptions;
+    char **options;
+    int *optionPresence;        // boolean array
     int numParameterOptions;
     char **parameterOptions;
     char **parameters;
+    int numOtherArgs;
     char **otherArgs;
-}
+} ArgTemplate;
 
 typedef enum ArgError {
     WRONG_PROGRAM_NAME,
-    UNKNOWN_SHORT_OPTION,
-    UNKNOWN_LONG_OPTION,
+    UNKNOWN__OPTION,
     UNKNOWN_PARAMETER_OPTION
 } ArgError;
 
@@ -33,25 +35,50 @@ copy_array_of_strings(char **dst, char **src, int numStrings)
 }
 
 void
-set_arg_template(
+init_arg_template(
     ArgTemplate *at,
     char *programName,
-    int numShortOptions,
-    char **shortOptions,
-    int numLongOptions,
-    char **numLongOptions,
+    int numOptions,
+    char **options,
+    int *optionPresence,
     int numParameterOptions,
-    char **parameterOptions
+    char **parameterOptions,
+    char **parameters
 )
 {
     at->programName = programName;
-    copy_array_of_strings(at->shortOptions, shortOptions, numShortOptions);
-    copy_array_of_strings(at->longOptions, longOptions, numLongOptions);
+    copy_array_of_strings(at->options, options, numOptions);
     copy_array_of_strings(at->parameterOptions, parameterOptions,
         numParameterOptions);
-    at->numShortOptions = numShortOptions;
-    at->numLongOptions = numLongOptions;
+
+    // set default presence values, or 0 (false) if default values are not
+    // passed
+    at->optionPresence = malloc(sizeof(int) * numOptions);
+    if (optionPresence != NULL) {
+        for (int i = 0; i<numOptions; i++) {
+            at->optionPresence[i] = optionPresence[i];
+        }
+    } else {
+        for (int i = 0; i<numOptions; i++) {
+            at->optionPresence[i] = 0;
+        }
+    }
+
+    // set default values, or NULL if default values are not passed
+    if (parameters != NULL) {
+        copy_array_of_strings(at->parameters, parameters,
+            numParameterOptions);
+    } else {
+        for (int i = 0; i<numParameterOptions; i++) {
+            at->parameterOptions[i] = NULL;
+        }
+    }
+
+    at->numOptions = numOptions;
     at->numParameterOptions = numParameterOptions;
+
+    at->numOtherArgs = 0;
+    at->otherArgs = NULL;
 }
 
 // if this returns 0, that means the option was not a parameterOption
@@ -71,28 +98,89 @@ int parameterLengthExtractor(char *arg, int argLength)
 }
 
 ArgError
-parse_args(ArgTemplate *template, ArgTemplate *as, int argc, char *argv[])
+parse_args(ArgTemplate *at, ArgTemplate *as, int argc, char *argv[])
 {
-    if (strcmp(argv[0], template->programName) != 0) {
+    if (strcmp(argv[0], at->programName) != 0) {
         return WRONG_PROGRAM_NAME;
     }
 
+    int numOtherArgs = 0;
+    init_arg_template(
+        as,
+        at->programName,
+        at->numOptions,
+        at->options,
+        at->optionPresence,
+        at->numParameterOptions,
+        at->parameterOptions,
+        at->parameters
+    );
+
     for (int i = 1; i<argc; i++) {
-        if (argv[i][0] == '-')
+        if (argv[i][0] == '-') {
             int stringLength = strlen(argv[i]);
             int parameterLength = parameterLengthExtractor(argv[i],
                                     stringLength);
 
             if (parameterLength != 0) {
                 // parameterOption
-            } else if (argv[i][1] == '-') {
-                // longOption
+                char *parameter = argv[i] + 1 + stringLength-parameterLength;
+
+                char *parameterOption = malloc(sizeof(char) * stringLength-parameterLength-1);
+                parameterOption[stringLength-parameterLength-2] = '\0';
+                strncpy(parameterOption, argv[i]+1, stringLength-parameterLength-2);
+
+                int debugFlag = 0;
+                for (int j = 0; j<at->numParameterOptions; j++) {
+                    if (strcmp(at->parameterOptions[j], parameterOption) == 0) {
+                        as->parameters[j] = parameter;
+                        debugFlag = 1;
+                        break;
+                    }
+                }
+
+                if (!debugFlag) {
+                    perror("parameter not found in struct, but there as arg\n");
+                    exit(1);
+                }
             } else {
-                // shortOption
+                // option
+                char *option = malloc(sizeof(char) * stringLength-parameterLength);
+                option[stringLength-parameterLength-1] = '\0';
+                strncpy(option, argv[i]+1, stringLength-parameterLength-1);
+
+                int debugFlag = 0;
+                for (int j = 0; j<at->numOptions; j++) {
+                    if (strcmp(at->options[j], option) == 0) {
+                        as->optionPresence[j] = 1;
+                        debugFlag = 1;
+                        break;
+                    }
+                }
+
+                if (!debugFlag) {
+                    perror("option not found in struct, but there as arg\n");
+                    exit(1);
+                }
             }
         } else {
-            // otherArgs
+            numOtherArgs++;
+        }
+    }
+
+    as->numOtherArgs = numOtherArgs;
+    as->otherArgs = malloc(sizeof(char *) * numOtherArgs);
+    int otherArgIdx = 0;
+    for (int i = 1; i<argc; i++) {
+        if (argv[i][0] != '-') {
+            int otherArgLen = strlen(argv[i]);
+            as->otherArgs[otherArgIdx] = malloc(sizeof(char) * (otherArgLen));
+            strcpy(as->otherArgs[otherArgIdx], argv[i]+1);
         }
     }
 }
 
+int main() {
+    printf("Hello World");
+    return 0;
+}
